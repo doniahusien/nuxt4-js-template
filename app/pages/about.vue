aaf091f9dc417a79ecac5b462bec8a5abbe7da3d
doniahusien <doniahusien@gmail.com>
2025-10-04 15:11:50 +0300

    Initial Nuxt 4 JS Template

---
+++ app/pages/about.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>About</h1>
+    <h1>HELOO</h1>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+   <p>{{ x }}</p> 
+    <NuxtLink to="/about">Go to About</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+
+let x = "hello";
+</script>
